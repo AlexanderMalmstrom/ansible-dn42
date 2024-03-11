e4f62adf11ce9bcb20056e86103b6b4d7105bc74
Markus Lehtimäki <7189417+Lehtimaki@users.noreply.github.com>
2024-03-11 12:17:43 +0100

    Use has_key to detect if a key exists in the config or not

--- checker.py
+++ checker.py
@@ -8,56 +8,16 @@ import requests
 
 KNOWN_ROUTER_FILES = ["de1.yml", "se1.yml", "uk1.yml"]
 
-def asn_exists(asn):
-    r = requests.get(f"https://explorer.burble.com/api/registry/aut-num/AS{asn}", timeout=10)
-    return r.status_code == 200
-
-def validate_peer_config(config):
-    if not config["name"]:
-        raise Exception("Missing name")
-
-    if not config["name"].startswith("dn42_"):
-        raise Exception("Invalid name")
-
-    if not config["remote"]:
-        raise Exception("Missing remote")
-
-    remote = config["remote"].partition(":")[0]
-    if not validators.domain(remote) or validators.ipv4(remote) or validators.ipv6(remote):
-        raise Exception("Invalid remote")
-
-    if not config["local_v6"] and not config["local_v4"]:
-        raise Exception("Missing local_v4 or local_v6")
-
-    if config["local_v6"] and not validators.ipv6(config["local_v6"]):
-        raise Exception("Invalid local_v6")
-
-    if config["local_v4"] and not validators.ipv4(config["local_v4"]):
-        raise Exception("Invalid local_v4")
-
-    if not config["peer_v6"] and not config["peer_v4"]:
-        raise Exception("Missing peer_v4 or peer_v6")
-
-    if config["peer_v6"] and not validators.ipv6(config["remote_v6"]):
-        raise Exception("Invalid peer_v6")
-
-    if config["peer_v4"] and not validators.ipv4(config["remote_v4"]):
-        raise Exception("Invalid peer_v4")
-
-    if config["asn"] and not asn_exists(config["asn"]):
-        raise Exception("Invalid asn")
-
-
 if __name__ == "__main__":
-    for router in os.listdir("conf"):
-        if router not in KNOWN_ROUTER_FILES:
-            print(f"'{file}' is not a legal file, skipping")
+    for file in os.listdir("conf"):
+        if file not in KNOWN_ROUTER_FILES:
+            print(f"Unknown file '{file}', skipping")
             continue
 
-        print(router)
+        print(file)
 
-        with open("conf/"+ router, 'r', encoding="utf-8") as file:
-            peers = yaml.safe_load(file)
+        with open(f"conf/{file}", 'r', encoding="utf-8") as config:
+            peers = yaml.safe_load(config)
             for peer in peers["wg_peers"]:
                 try:
                     validate_peer_config(peer)
@@ -67,3 +27,47 @@ if __name__ == "__main__":
 
     print("Valid configuration")
     sys.exit(0)
+
+# Check if an ASN exists
+def asn_exists(asn):
+    r = requests.get(f"https://explorer.burble.com/api/registry/aut-num/AS{asn}", timeout=10)
+    return r.status_code == 200
+
+# Validates used keys of a peer config
+def validate_peer_config(config):
+    if not config.has_key("name") or not config["name"]:
+        raise Exception("Missing name")
+
+    if not config["name"].startswith("dn42_"):
+        raise Exception("Invalid name")
+
+    if not config.has_key("name") or not config["remote"]:
+        raise Exception("Missing remote")
+
+    if not config.has_key("remote") or not config["remote"]:
+        raise Exception("Missing remote")
+
+    remote = config["remote"].partition(":")[0]
+    if not validators.domain(remote) or validators.ipv4(remote) or validators.ipv6(remote):
+        raise Exception("Invalid remote")
+
+    if not config.has_key("local_v6") and not config.has_key("local_v4"):
+        raise Exception("Missing one of local_v4, local_v6")
+
+    if config.has_key("local_v6") and not validators.ipv6(config["local_v6"]):
+        raise Exception("Invalid local_v6")
+
+    if config.has_key("local_v4") and not validators.ipv4(config["local_v4"]):
+        raise Exception("Invalid local_v4")
+
+    if not config.has_key("peer_v6") and not config["peer_v4"]:
+        raise Exception("Missing peer_v4 or peer_v6")
+
+    if config.has_key("peer_v6") and not validators.ipv6(config["remote_v6"]):
+        raise Exception("Invalid peer_v6")
+
+    if config.has_key("peer_v4") and not validators.ipv4(config["remote_v4"]):
+        raise Exception("Invalid peer_v4")
+
+    if config.has_key("asn") and not asn_exists(config["asn"]):
+        raise Exception("Invalid asn")
