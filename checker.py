8f4ca272da656981fc2d5017fc40112580ac570c
Markus Lehtimäki <7189417+Lehtimaki@users.noreply.github.com>
2024-03-11 11:57:21 +0100

    Do some cleanup of validation code

--- checker.py
+++ checker.py
@@ -2,77 +2,68 @@
 
 import os
 import sys
-import pprint
 import yaml
 import validators
 import requests
 
-pp = pprint.PrettyPrinter(indent=4)
-legalKeys = [
-    'name', 'remote',
-    'wg_pubkey', 'peer_v6',
-    'peer_v4', 'local_v6',
-    'local_v4', 'asn',
-    'description', 'port' 
-    ]
-legalfiles = ['de1.yml', 'se1.yml', 'uk1.yml']
+KNOWN_ROUTER_FILES = ["de1.yml", "se1.yml", "uk1.yml"]
 
-# Get the list of all peers
-dir_list = os.listdir("conf")
-#print(dir_list)
+def asn_exists(asn):
+    r = requests.get(f"https://explorer.burble.com/api/registry/aut-num/AS{asn}", timeout=10)
+    return r.status_code == 200
 
-for file in dir_list:
-    if file not in legalfiles:
-        print(file, "is not a legal file, please delete it")
-        sys.exit(1)
+def validate_peer_config(config):
+    if not config["name"]:
+        raise Exception("Missing name")
 
-for router in dir_list:
-    print(router)
+    if not config["name"].startswith("dn42_"):
+        raise Exception("Invalid name")
 
-    with open("conf/"+ router, 'r', encoding="utf-8") as file:
-        peers = yaml.safe_load(file)
-        peers = peers['wg_peers']
-        for peer in peers:
-            for key, value in peer.items():
+    if not config["remote"]:
+        raise Exception("Missing remote")
 
-                if key not in legalKeys:
-                    print("Oh nose,", key, "isn't a legal key")
+    remote = config["remote"].partition(":")[0]
+    if not validators.domain(remote) or validators.ipv4(remote) or validators.ipv6(remote):
+        raise Exception("Invalid remote")
+
+    if not config["local_v6"] and not config["local_v4"]:
+        raise Exception("Missing local_v4 or local_v6")
+
+    if config["local_v6"] and not validators.ipv6(config["local_v6"]):
+        raise Exception("Invalid local_v6")
+
+    if config["local_v4"] and not validators.ipv4(config["local_v4"]):
+        raise Exception("Invalid local_v4")
+
+    if not config["peer_v6"] and not config["peer_v4"]:
+        raise Exception("Missing peer_v4 or peer_v6")
+
+    if config["peer_v6"] and not validators.ipv6(config["remote_v6"]):
+        raise Exception("Invalid peer_v6")
+
+    if config["peer_v4"] and not validators.ipv4(config["remote_v4"]):
+        raise Exception("Invalid peer_v4")
+
+    if config["asn"] and not asn_exists(config["asn"]):
+        raise Exception("Invalid asn")
+
+
+if __name__ == "__main__":
+    for router in os.listdir("conf"):
+        if router not in KNOWN_ROUTER_FILES:
+            print(f"'{file}' is not a legal file, skipping")
+            continue
+
+        print(router)
+
+        with open("conf/"+ router, 'r', encoding="utf-8") as file:
+            peers = yaml.safe_load(file)
+            for peer in peers["wg_peers"]:
+                try:
+                    validate_peer_config(peer)
+                except Exception as e:
+                    print("Peer config validation:", e)
                     sys.exit(1)
-                if key == "name":
-                    if not value:
-                        print("Name cannot be empty")
-                        sys.exit(1)
 
-                    if not value.startswith('dn42_'):
-                        print('Name should start with the prefix dn42_')
-                        sys.exit(1)
-
-                #this should be a IP or an URL, lets test that.
-                if key == "remote":
-                    value = value.partition(':')[0]
-                    if (not validators.domain(value) or
-                        validators.ipv4(value) or
-                        validators.ipv6(value)):
-                        print(value, "is invalid remote, should be domain or IP")
-                        sys.exit(1)
-
-                if key in ("peer_v6", "local_v6"):
-                    if not validators.ipv6(value):
-                        print("Invalid ", key, " this should be IPv6")
-                        sys.exit(1)
-                if key in ("peer_v4", "local_v4") :
-                    if not validators.ipv4(value):
-                        print("Invalid ", key, " this should be IPv4")
-                        sys.exit(1)
-
-                if key == "asn":
-                    r = requests.get(
-                        'https://explorer.burble.com/api/registry/aut-num/AS' + str(value), 
-                        timeout=10)
-                    if r.status_code != 200:
-                        print("ASN don't exists")
-                        sys.exit(1)
-
-
-print("all tests looks good")
-sys.exit(0)
+    print("Valid configuration")
+    sys.exit(0)
